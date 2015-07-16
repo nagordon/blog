6aca68b303006f560986c7b7b2b3ec92bd73fcb1
Neal Gordon <nealagordon@gmail.com>
2015-07-16 13:06:43 -0400

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 #SITEURL = 'http://nagordon.github.io'
-SITEURL = 'ifcuriousthenlearn.com'
+SITEURL = 'http://ifcuriousthenlearn.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
