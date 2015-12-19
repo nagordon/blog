0a0ed72b1ff7358e4dcbe9991d3b96cba6ce37d8
Neal Gordon <nealagordon@gmail.com>
2015-12-19 18:19:44 +0000

    updated code with custom domain

--- make_site.py
+++ make_site.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 17 18:49:47 2015
-
-@author: neal
 
 # delete remote branch
 git push origin --delete gh-pages
 
+# delete local branch 
+git branch -D gh-pages
 
-"""
 
-import os
+### generate site
+pelican content -s pelicanconf.py
 
-#os.system('pelican content -s pelicanconf.py')
+### create a development server that updates when changes are detected
+make devserver
+# stop 
+./develop_server.sh stop
 
+
+# generate the site and push it to github
+make github
+
+
+
+### automatically create CNAME file
 cmds = ["git checkout gh-pages",
 		"echo 'ifcuriousthenlearn.com' > CNAME",
 	    "git add CNAME",
@@ -22,3 +31,12 @@ cmds = ["git checkout gh-pages",
 
 for k in cmds:
 	os.system(k)
+	
+	
+"""
+
+import os
+
+pelican content -o output -s pelicanconf.py
+ghp-import output
+git push origin gh-pages	
