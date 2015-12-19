ffa1fb13f238b6fd19124265194025da71e730f6
Neal Gordon <nealagordon@gmail.com>
2015-12-19 18:11:24 +0000

    writing script to create CNAME file

--- make_site.py
+++ make_site.py
@@ -3,9 +3,22 @@
 Created on Fri Jul 17 18:49:47 2015
 
 @author: neal
+
+# delete remote branch
+git push origin --delete gh-pages
+
+
 """
 
 import os
 
-os.system('pelican content -s pelicanconf.py')
+#os.system('pelican content -s pelicanconf.py')
 
+cmds = ["git checkout gh-pages",
+		"echo 'ifcuriousthenlearn.com' > CNAME",
+	    "git add CNAME",
+		"git commit -m 'Added CNAME'",
+		"git push origin gh-pages"]
+
+for k in cmds:
+	os.system(k)
